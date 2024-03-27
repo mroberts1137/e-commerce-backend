5332efad5f726d08195007afe2fe1758800158e2
mroberts1137 <mroberts1137@gmail.com>
2024-03-27 18:40:35 -0400

    products and products/productId endpoints

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,19 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../authenticate');
 
-router.route('/').get((req, res) => {
-  res.end('Fetching all products');
-});
+/**
+ * Route: localhost:3000/products'
+ */
+
+router
+  .route('/')
+  .get((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'plain/text');
+    res.end('Sending all products');
+  })
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, status: 'Posted successfully!' });
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /products');
+  })
+  .delete((req, res) => {
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /products');
+  });
+
+/**
+ * Route: localhost:3000/products/:productId'
+ */
 
 router
   .route('/:productId')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
   .get((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'plain/text');
     res.end(`Fetching product ${req.params.productId}`);
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /products/${req.params.productId}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /products/${req.params.productId}`);
+  })
+  .delete((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `DELETE operation not supported on /products/${req.params.productId}`
+    );
   });
 
 module.exports = router;
