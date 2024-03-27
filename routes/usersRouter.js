fe851c73114c496ea8c6ef0a2eafdf6fd8a97e70
mroberts1137 <mroberts1137@gmail.com>
2024-03-26 20:18:14 -0400

    add users route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,8 +5,64 @@ const authenticate = require('../authenticate');
 
 const User = require('../models/user');
 
-router.get('/', (req, res, next) => {
-  res.end('Users');
+router.get(
+  '/',
+  authenticate.verifyUser,
+  authenticate.verifyAdmin,
+  (req, res, next) => {
+    User.find()
+      .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+      })
+      .catch((err) => next(err));
+  }
+);
+
+router.post('/signup', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        req.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      } else {
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' });
+          });
+        });
+      }
+    }
+  );
 });
 
-module.exports = router;
+userRouter.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({
+    success: true,
+    token: token,
+    status: 'You are successfully logged in!'
+  });
+});
+
+module.exports = userRouter;
